2e1bf03e34eb410342a634a391352d5fe6d5521a
unknown <sumitkumarsingh309@gmail.com>
2018-04-20 23:47:46 -0700

    first commit

---
+++ app/src/main/java/com/example/sanny/loader/CutomerMenu_Activity.java
@@ -0,0 +1,13 @@
+package com.example.sanny.loader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CutomerMenu_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cutomer_menu_);
+    }
+}
